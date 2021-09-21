9fd62a2167cbd2e168f5dd3da571be4fde5c8b11
MyintSanWin <jackmee24680@gmail.com>
2021-09-21 23:29:47 +0630

    learn variable and template strings and default arguments and symbol and arrow function

--- script.js
+++ script.js
@@ -43,3 +43,66 @@ function moveCommand(direction){
     }
     return whatHappens;
 }
+
+
+const player = 'bobby';//can't chage value  as using assign
+let experience = 100;
+let wizardLevel = false;
+
+if (experience = 90){
+    let wizardLevel = true;
+    console.log('inside', wizardLevel);
+}
+console.log('outside', wizardLevel);
+
+
+//can change in obj
+const obj = {
+    player: 'bobby',
+    experience:100,
+    wizardLevel:false
+}
+
+const player = obj.player;
+const experience = obj.experience;
+const wizardLevel = obj.wizardLevel;
+
+const { player, experience, } = obj;
+
+
+//template strings
+const name = "Sally";
+const age = 38;
+const pet = "horse";
+
+const greeting = "Hello" + name + " you seem to be doing " + greeting + "!";
+const greetingBest = "Hello ${name} you seem to be ${age=15}. What a lovely ${pet} you have";
+
+
+//default arguments
+
+function greet(name='', age=23, pet='cat')
+{
+    return    "Hello ${name} you seem to be ${age=15}. What a lovely ${pet} you have";
+
+}
+
+//symbol
+
+var sym1 = Symbol();
+var sym2 = Symbol('foo');
+var sym3 = Symbol('foo');
+
+//arrow function
+
+function add(a,b){
+    return a + b;
+}
+
+const add = (a, b) => a + b;
+ or 
+
+
+ const add = (a, b) => {
+   return  a + b;
+ }

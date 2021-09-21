891b599c4e0abd4f0bbaa524fcde09afd822c75b
MyintSanWin <jackmee24680@gmail.com>
2021-09-21 21:37:53 +0630

    ternary operator

---
+++ script.js
@@ -0,0 +1,18 @@
+//ternary operator 
+// condition ? expr1 : expr2;
+function isUserValid(bool) {
+    return bool;
+}
+var answer = isUserValid(true) ? "You may enter" : "Access Denied";
+
+
+function condition(){
+    if(isUserValid(true)){
+        return "You may enter";
+
+    }
+    else{
+        return "Access denied";
+    }
+}
+var answer2 = condition();

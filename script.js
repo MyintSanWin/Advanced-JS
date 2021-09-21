abdecab777094466a977139ef3e3db528d9139ca
MyintSanWin <jackmee24680@gmail.com>
2021-09-21 21:44:35 +0630

    switch case condition

--- script.js
+++ script.js
@@ -16,3 +16,30 @@ function condition(){
     }
 }
 var answer2 = condition();
+
+function moveCommand(direction){
+    var whatHappens;
+    switch(direction){
+        case "forward":
+         whatHappens = "you encounter a monster";
+         break;
+
+         case "back":
+            whatHappens = "you arrived home";
+            break;
+         
+            case "right":
+                whatHappens = "you found a river";
+                break;
+
+                case "left":
+                    whatHappens = "you run into a troll ";
+                    break;
+                    default:
+                        whatHappens = "please enter a valid direction";
+                    
+                
+
+    }
+    return whatHappens;
+}
